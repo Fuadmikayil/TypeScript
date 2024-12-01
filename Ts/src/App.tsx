00b4359491385adbfe2eb4e5f669bfd4c998c2c5
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-01 11:08:14 +0400

    first commit

---
+++ Ts/src/App.tsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  let a:number = 5
+  const demo = () =>{
+
+  }
+  return (
+    <div>
+      <h1>TypeScript</h1>
+      <h1>Vlue {a} </h1>
+      <button onClick={demo}>Change Value</button>
+      </div>
+  )
+}
+
+export default App
