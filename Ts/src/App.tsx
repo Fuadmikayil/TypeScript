fb9933fd7f6086158f284308f29b3d017e6af19d
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-01 11:33:20 +0400

    type

--- Ts/src/App.tsx
+++ Ts/src/App.tsx
@@ -1,14 +1,30 @@
 
 const App = () => {
-  let a:number = 5
-  const demo = () =>{
-
+  type Person = {
+    fullname:string;
+    age:number;
+    salary:number;
+    isWorking:boolean;
+    isMarrt: boolean;
+    speciality:string;
+  }
+  const calculateİpoteca = (client:Person) =>{
+      console.log(client);
+      
+  }
+  const person:Person = {
+    fullname: "Zahid ",
+    age:14,
+    salary:4000,
+    isWorking:true,
+    isMarrt: true,
+    speciality:"string",
   }
   return (
     <div>
       <h1>TypeScript</h1>
       <h1>Vlue {a} </h1>
-      <button onClick={demo}>Change Value</button>
+      <button onClick={() => { calculateİpoteca(person)}}>Change Value</button>
       </div>
   )
 }
